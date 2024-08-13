2e66a1d75285c702dee7b02bb921b63138127cad
abdulrahmanDev1 <abdohamdy198461@gmail.com>
2024-08-13 18:38:47 +0300

    fix qa on mobile

--- app/questions/page.tsx
+++ app/questions/page.tsx
@@ -7,7 +7,7 @@ export const metadata = {
 
 export default function Page() {
   return (
-    <main className="flex gap-4 flex-col items-center justify-between px-24">
+    <main className="flex gap-4 flex-col items-center justify-between px-4 md:px-12 lg:px-24">
       <h1 className="lg:text-6xl md:text-4xl text-3xl">Question & Answers</h1>
       <h4 className="text-md md:textlg lg:text-xl text-gray-400">
         Hover/Click the question to see the answer
