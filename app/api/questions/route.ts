d42c7edaa310b93f5efd1d5918d0ef26ce94d874
abdulrahmanDev1 <abdohamdy198461@gmail.com>
2024-08-14 17:16:48 +0300

    force dynamic
    im tired of this the data just dosent want to be updated

--- app/api/questions/route.ts
+++ app/api/questions/route.ts
@@ -1,6 +1,6 @@
 import { getQuestionsWithAnswers } from "@/server/queries";
 import { NextRequest, NextResponse } from "next/server";
-
+export const dynamic = "force-dynamic";
 export async function GET(req: NextRequest) {
   const questions = await getQuestionsWithAnswers();
   return NextResponse.json(questions);
